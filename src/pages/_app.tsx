1424dc6bb999df1009b3d7b10867260e6fec9267
boilerrat <128boilerrat@gmail.com>
2026-03-29 19:45:38 -0400

    restore app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+
+import { ErrorBoundary } from "../components/ErrorBoundary";
+import { ThemeProvider } from "../components/ThemeProvider";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  void session;
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={session}>
+      <ThemeProvider>
+        <ErrorBoundary>
+          <Component {...pageProps} />
+        </ErrorBoundary>
+      </ThemeProvider>
+    </SessionProvider>
+  );
 }
